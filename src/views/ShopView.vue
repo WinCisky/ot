e52e7b2fcd0c0a2b30e03fac5324fb4d8fc34fe1
Simone <xsimone97@hotmail.it>
2022-10-05 22:25:25 +0200

    first commit

---
+++ src/views/ShopView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="shop">
+      <h1>This is shop {{ $route.params }}</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
