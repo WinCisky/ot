492990d82b18bed73deb009a60112623c9e78624
Simone <xsimone97@hotmail.it>
2022-10-06 23:10:48 +0200

    load shop name

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,16 +1,74 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+      
 <template>
-    <div class="shop">
-      <h1>This is shop {{ $route.params }}</h1>
-    </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
+  <h1>{{ name }}</h1>
+</template>
+      
+      
+<script lang="ts">
+import { supabase } from '../supabase'
+import type { definitions } from '../supabase_types';
+
+export default {
+  data() {
+    return {
+      name: "",
+      reviews_first_row: [] as definitions['shop_reviews'][],
+      reviews_second_row: [] as definitions['shop_reviews'][],
+      reviews_third_row: [] as definitions['shop_reviews'][],
+    }
+  },
+  mounted() {
+    this.loadShopData();
+    this.loadShopReviews();
+  },
+  methods: {
+    async loadShopData() {
+      let shop = this.$route.params.shop
+
+      if (typeof shop === "string") {
+        const result = await supabase
+          .from<definitions['shop_names']>('shop_names')
+          .select()
+          .eq('shop', shop)
+
+        if (
+          result &&
+          result.data?.length &&
+          result.data?.length > 0 &&
+          result.data[0].name
+        )
+          this.name = result?.data[0].name;
+      }
+
+    },
+    async loadShopReviews() {
+      // let shop = this.$route.params.shop
+
+      // if (typeof shop === "string") {
+      //   const result = await supabase
+      //     .from<definitions['shop_reviews']>('shop_reviews')
+      //     .select()
+      //     .eq('shop', shop)
+      //     .range(0,15)
+
+      //   if (
+      //     result &&
+      //     result.data?.length &&
+      //     result.data?.length > 0 &&
+      //     result.data[0].name
+      //   )
+      //     this.name = result?.data[0].name;
+      // }
     }
   }
-  </style>
-  
+}
+</script>
+      
+<style scoped>
+
+</style>
